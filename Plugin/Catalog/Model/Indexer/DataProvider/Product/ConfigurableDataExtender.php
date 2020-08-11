46780bffa05f71133bb029b1cc738ad94e36830f
Pablo Giralt <pablogiralt@gmail.com>
2020-08-11 10:06:49 +0200

    fix: adapt to vsbridge latest version (1.20.1)

--- Plugin/Catalog/Model/Indexer/DataProvider/Product/ConfigurableDataExtender.php
+++ Plugin/Catalog/Model/Indexer/DataProvider/Product/ConfigurableDataExtender.php
@@ -262,7 +262,7 @@ class ConfigurableDataExtender {
         $configurableAttributeCodes = $subject->getConfigurableResource()->getConfigurableAttributeCodes();
 
         $allChildren = $subject->getChildrenAttributeProcessor()
-            ->loadChildrenRawAttributesInBatches($storeId, $allChildren, $configurableAttributeCodes);
+            ->execute($storeId, $allChildren, $configurableAttributeCodes);
 
         // add Media Gallery
         $allChildren = $mediaGalleryDataProvider->addData($allChildren, $storeId);
